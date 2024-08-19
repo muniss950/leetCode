374be401d30d2ef87aeaaeaa37d536bda7402cd5
muniss950 <munish3971@gmail.com>
2024-08-19 16:24:34 +0530

    done

--- findlongestchain.cpp
+++ findlongestchain.cpp
@@ -1,6 +1,32 @@
+#include<bits/stdc++.h>
+#include <utility>
+#include <vector>
+
+using namespace std;
 class Solution {
 public:
-    int findLongestChain(vector<vector<int>>& pairs) {
-        
+  static  bool compare(const vector<int>&a,const vector<int>&b){
+        return a[0]<b[0];
+    }
+  int helper(vector<vector<int>>pairs,int curr,int pos){
+    cout<<curr<<endl;
+    if(pos>=pairs.size()-1){
+      return curr;
+    }
+    int max=0;
+    for(int i=pos+1;i<pairs.size();i++){
+      int temp=0;
+      if(pairs[pos][1]<pairs[i][0]){
+        temp=helper(pairs, curr+1, i);
+      }
+      if(temp>max){
+        max=temp;
+      }
+    }
+    return max;
+  }
+    int findLongestChain(vector<vector<int>>& pairs) {    
+      sort(pairs.begin(),pairs.end(),compare);
+      return helper(pairs, 1, 0);
     }
 };
