df450a14055b76246bde7c6eed5d4f3bbf719040
muniss950 <munish3971@gmail.com>
2024-08-21 14:21:00 +0530

    lol

--- coinchange2.cpp
+++ coinchange2.cpp
@@ -4,29 +4,44 @@
 using namespace std;
 class Solution {
 public:
-  int helper(int amount, vector<int> coins, int pos, vector<int> &memo) {
-    if (amount == 0) {
-      return 1;
-    }
+  int helper(int amount, vector<int> coins, int pos, vector<vector<int>> &memo) {
+    cout << amount << " ";
     if (amount < 0) {
+      cout << "lol" << endl;
       return 0;
     }
-    if(memo[amount]!=-1){
-      return memo[amount];
+    if(memo[amount][pos]!=-1){
+        cout<<endl;
+      return memo[amount][pos];
     }
-
-    int sum = 0;
+    if (amount == 0) {
+      cout << endl;
+      return 1;
+    }
+    long long int sum = 0;
     for (int i = pos; i < coins.size(); i++) {
-      sum += helper(amount - coins[i], coins, i,memo);
+      sum += helper(amount - coins[i], coins, i, memo);
     }
+    memo[amount][pos] = sum;
     return sum;
   }
   int change(int amount, vector<int> &coins) {
-    vector<int> memo(amount + 1, -1);
-    for (int i = 1; i < amount + 1; i++) {
-
-      memo[i] = helper(amount, coins, 0,memo);
+    vector<vector<int>> memo(amount + 1, vector(coins.size(), -1));
+    for (int i = 0; i < coins.size(); i++) {
+      memo[0][i] = 1;
     }
-    return memo[amount];
+    for (int i = 0; i < amount + 1; i++) {
+      cout << i << ": " << endl;
+      for (int j = 0; i < coins.size(); j++) {
+        memo[i][j] = helper(i, coins,j, memo);
+      }
+      //   cout<<memo[i]<<endl;
+    }
+    int sum=0;
+
+    for(int i=0;i<coins.size();i++){
+      sum+=memo[amount][i];
+    }
+    return sum;
   }
 };
--- mincosttickets.cpp
+++ mincosttickets.cpp
@@ -5,18 +5,45 @@
 using namespace  std;
 class Solution {
 public:
-  int helper(vector<int>days,vector<int>costs,int pos){
-    if(days.back()<pos){
+  int helper(vector<int>days,vector<int>costs,int pos,vector<int>&memo){
+    cout<<pos<<endl;
+    if(pos>=days.size()){
       return 0;
     }
     
-    int ch1=helper(days,costs,pos+1)+costs[0];
-    int ch2=helper(days,costs, pos+7)+costs[1];
-    int ch3=helper(days,costs, pos+30)+costs[2];
+    if(memo[pos]!=-1){
+      return memo[pos];
+    }
+    int ch1=helper(days,costs,pos+1,memo)+costs[0];
+
+    int ind=pos;
+    int curr=days[pos];
+    while(curr+7>days[ind]){
+      ind++;
+    if(ind>=days.size()){
+        break;
+      }
+    }
+    int ch2=helper(days,costs,ind,memo)+costs[1];
+    ind=pos;
+    curr=days[pos];
+    while(curr+30>days[ind]){
+      ind++;
+      if(ind>=days.size()){
+        break;
+      }
+    }
+    int ch3=helper(days,costs, ind,memo)+costs[2];
     return min(ch1,min(ch2,ch3));
   }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+      vector<int>memo(days.size()+1,-1);
+      memo[days.size()]=0;
+      for(int i=days.size()-1;i>=0;i--){
+        memo[i]=helper(days,costs,0,memo);
+
+      }
         
-      return helper(days,costs,0);
+      return memo[0];
     }
 };
