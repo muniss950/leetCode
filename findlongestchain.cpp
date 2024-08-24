131959bfec6753c45cb50dac4271328dff60b014
muniss950 <munish3971@gmail.com>
2024-08-24 18:41:12 +0530

    null

--- findlongestchain.cpp
+++ findlongestchain.cpp
@@ -6,31 +6,29 @@ using namespace std;
 class Solution {
 public:
   static bool compare(const vector<int> &a, const vector<int> &b) {
-    if (a[0] == b[0]) {
-      return a[1] < b[1];
+    if (a[1] == b[1]) {
+      return a[0] < b[0];
     }
-    return a[0] < b[0];
+    return a[1] < b[1];
   }
-  int helper(vector<vector<int>> pairs, int curr, int pos,vector<int>&memo) {
-    if (pos >= pairs.size() - 1) {
+  int helper(vector<vector<int>> pairs, int curr, int pos,int prev,vector<int>&memo) {
+    if (pos >= pairs.size() ) {
       return curr;
     }
-    if(memo[pos]!=-1){
+
+    if(prev==-1){
+      memo[pos]=helper(pairs,curr+1,pos+1,pos,memo);
       return memo[pos];
     }
-    // cout<<pairs[pos][0]<<" "<<pairs[pos][1]<<endl;
-    int max = 0;
-    for (int i = pos + 1; i < pairs.size(); i++) {
-      int temp = 0;
-      if (pairs[pos][1] < pairs[i][0]) {
-        temp = helper(pairs, curr + 1, i,memo);
-      }
-      if (temp > max) {
-        max = temp;
-      }
+    if(pairs[prev][1]<pairs[pos][0] &&memo[pos]!=-1){
+      return memo[pos]+curr;
     }
-    memo[pos]=max;
-    return max;
+    int temp=helper(pairs,curr,pos+1,prev,memo);
+    if(pairs[prev][1]<pairs[pos][0]){
+      temp=max(temp,helper(pairs,curr+1,pos+1,prev,memo));
+    }
+    memo[pos]=temp;
+    return memo[pos];
   }
   int findLongestChain(vector<vector<int>> &pairs) {
     if (pairs.size() == 1) {
@@ -40,12 +38,12 @@ public:
     vector<int> memo(pairs.size() + 1,-1);
     memo[pairs.size()]=0;
 
-    for (auto i : pairs) {
-      cout << i[0] << " " << i[1] << endl;
-    }
+    // for (auto i : pairs) {
+    //   cout << i[0] << " " << i[1] << endl;
+    // }
     int max = 0;
     for (int i = pairs.size()-1; i >=0; i--) {
-      int temp = helper(pairs, 1, i,memo);
+      int temp = helper(pairs, 0, i,-1,memo);
       if (temp > max) {
         max = temp;
       }
