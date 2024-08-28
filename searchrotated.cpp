33309ced2c8c8b7ca81c7841b5cabeb800d8d83b
muniss950 <munish3971@gmail.com>
2024-08-28 16:03:21 +0530

    lol

--- searchrotated.cpp
+++ searchrotated.cpp
@@ -1,10 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        
+  int search(vector<int> &nums, int target) {
+    int k = 0;
+    int i = 0;
+    while ((i + 1) < nums.size() && nums[i] < nums[i + 1]) {
+      k++;
     }
+    int start=k;
+    int end=k+nums.size();
+    while(start<=end){
+      int mid=(start+end)/2;
+
+    }
+  }
 };
